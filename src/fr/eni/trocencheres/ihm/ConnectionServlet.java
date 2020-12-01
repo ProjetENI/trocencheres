9ce07c47d3cc1214f2779a816f2b58a75200a2d0
Julien <Julien@CYRIL-PC>
2020-12-01 16:15:03 +0100

    doGet et doPost

--- src/fr/eni/trocencheres/ihm/ConnectionServlet.java
+++ src/fr/eni/trocencheres/ihm/ConnectionServlet.java
@@ -1,29 +1,39 @@
 package fr.eni.trocencheres.ihm;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
+
 @WebServlet("/ConnectionServlet")
 public class ConnectionServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	private static String REDIRECTIONLOGIN = "login.jsp";
-	private static String REDIRECTIONINDEX = "index.jsp";
+	private static final String INDEX = "Index";
+	private static final String LOGIN = "Login";
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		response.sendRedirect(REDIRECTIONLOGIN);
+		forward(request, response, LOGIN);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		response.sendRedirect(REDIRECTIONINDEX);
+		forward(request, response, INDEX);
+
 	}
 
+	private void forward(HttpServletRequest request, HttpServletResponse response, String redirection) throws ServletException, IOException {
+
+		RequestDispatcher rd = this.getServletContext().getNamedDispatcher(redirection);
+		rd.forward(request, response);
+	}
 }
