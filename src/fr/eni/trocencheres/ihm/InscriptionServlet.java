8b5d85ddc893d387cc0980ebb48c051b746c4e97
FraLeclere <72281126+FraLeclere@users.noreply.github.com>
2020-12-02 10:21:20 +0100

    Servlet inscription + mapping

---
+++ src/fr/eni/trocencheres/ihm/InscriptionServlet.java
@@ -0,0 +1,39 @@
+package fr.eni.trocencheres.ihm;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+@WebServlet("/InscriptionServlet")
+public class InscriptionServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	private static final String INDEX = "Index";
+	private static final String INSCRIPTION = "Inscription";
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		forward(request, response, INSCRIPTION);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		forward(request, response, INDEX);
+
+	}
+
+	private void forward(HttpServletRequest request, HttpServletResponse response, String redirection) throws ServletException, IOException {
+
+		RequestDispatcher rd = this.getServletContext().getNamedDispatcher(redirection);
+		rd.forward(request, response);
+	}
+}
