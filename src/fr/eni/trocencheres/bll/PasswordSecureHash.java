46daf0991b19176fdce32cb824934e5c69279d7e
damienpuaud <damienpuaud@localhost>
2020-12-02 09:18:38 +0100

    Create Classe PasswordSecure

---
+++ src/fr/eni/trocencheres/bll/PasswordSecureHash.java
@@ -0,0 +1,8 @@
+package fr.eni.trocencheres.bll;
+
+public class PasswordSecureHash {
+
+	// CLASSE QUI SERVIRA À CRYPTER LE MOT DE PASSE
+	// METHODE À UTILISER : MD5 HASH
+	// SÉCURITÉ AMÉLIORÉE AVEC SALT
+}
