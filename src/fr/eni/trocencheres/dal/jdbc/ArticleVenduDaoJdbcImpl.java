ba4863df818385fcb4eea8b5358acec6493fc999
FraLeclere <72281126+FraLeclere@users.noreply.github.com>
2020-12-10 19:03:25 +0100

    Merci David

--- src/fr/eni/trocencheres/dal/jdbc/ArticleVenduDaoJdbcImpl.java
+++ src/fr/eni/trocencheres/dal/jdbc/ArticleVenduDaoJdbcImpl.java
@@ -24,7 +24,7 @@ import fr.eni.trocencheres.dal.ConnectionProvider;
 			+ "INNER JOIN UTILISATEURS as U ON A.no_utilisateur=U.no_utilisateur WHERE etat_vente='EC';";
 	
 	private final String SELECT_INFORMATIONS_ARTICLES = "SELECT * FROM ARTICLES_VENDUS as A INNER JOIN CATEGORIES as C ON A.no_categorie=C.no_categorie "
-			+ "INNER JOIN UTILISATEURS as U ON A.no_utilisateur=U.no_utilisateur WHERE etat_vente='EC' AND no_articlevendu='?';";
+			+ "INNER JOIN UTILISATEURS as U ON A.no_utilisateur=U.no_utilisateur WHERE etat_vente='EC' AND no_article='?';";
 	
 	private final String INSERT_ARTICLE_VENDU = "INSERT INTO ARTICLES_VENDUS"
 			+ "(nom_article, description, date_debut_enchere, date_fin_enchere, prix_initial, no_utilisateur, no_categorie, etat_vente, image) "
@@ -34,7 +34,7 @@ import fr.eni.trocencheres.dal.ConnectionProvider;
 			+"nom_article=?, description=?, prix_initial=? " 
 			+ "WHERE no_articlevendu=?;";
 	
-	private final String DELETE_ARTICLE_VENDU = "DELETE FROM ARTICLES_VENDUS WHERE no_articlevendu=?; ";
+	private final String DELETE_ARTICLE_VENDU = "DELETE FROM ARTICLES_VENDUS WHERE no_article=?; ";
 			
 
 
@@ -98,7 +98,6 @@ import fr.eni.trocencheres.dal.ConnectionProvider;
 		ArticleVendu informationArticleVendu = null;
 		try (	Connection conn = ConnectionProvider.getConnection();
 				PreparedStatement pstt = conn.prepareStatement(SELECT_INFORMATIONS_ARTICLES)) {
-			
 			
 			pstt.setInt(1, pNoArticle);
 			
