b299beaf9f9d096e64e8e8d30a9780cb10b5745c
FraLeclere <72281126+FraLeclere@users.noreply.github.com>
2021-01-05 19:43:56 +0100

    sdfdsf

---
+++ src/fr/eni/trocencheres/dal/EnchereDAO.java
@@ -0,0 +1,15 @@
+package fr.eni.trocencheres.dal;
+
+import fr.eni.trocencheres.BusinessException;
+import fr.eni.trocencheres.bo.Enchere;
+
+public interface EnchereDAO {
+
+	/**
+	 * Fonction prenant en paramètre un article pour l'ajouter en base de données
+	 * @param article vendu
+	 * @throws BusinessException 
+	 */
+	void ajouterEnchere(Enchere enchere) throws BusinessException;
+
+}
